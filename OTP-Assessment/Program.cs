0d8c83f583f14ed494b5d954a69917bb226bb0f5
Delos Alvin <alvindelos439@gmail.com>
2023-04-03 21:01:39 -0700

    Enable to input dst Email and src Email using args

--- OTP-Assessment/Program.cs
+++ OTP-Assessment/Program.cs
@@ -7,10 +7,22 @@ namespace OTP_Assessment
     {
         static void Main(string[] args)
         {
-            var otp = new Email_OPT_Module(args[0], args[1]);
+            Email_OPT_Module otp;
+            if (args.Length < 2)
+            {
+                otp = new Email_OPT_Module("peter.jamesds@outlook.com", "Test!234");
+            }
+            else
+            {
+                otp = new Email_OPT_Module(args[0], args[1]);
+            }
             otp.Start();
 
-            if (otp.GenerateOPTEmail("gribnekov.jeong@outlook.com") != Email_OPT_Module.EmailStatus.STATUS_EMAIL_OK)
+            Console.Write("Enter the email address to get OTP: ");
+            string dstEmail = Console.ReadLine().Trim();
+            dstEmail = dstEmail != "" ? dstEmail : "gribnekov.jeong@outlook.com";
+
+            if (otp.GenerateOPTEmail(dstEmail) != Email_OPT_Module.EmailStatus.STATUS_EMAIL_OK)
             {
                 Console.WriteLine("Fail");
                 return;
